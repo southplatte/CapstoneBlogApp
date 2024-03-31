a0bf308aef5685a9b18504a131239dcf087c6f93
Bill N <bnab@southplattewebdesign.com>
2024-03-30 19:44:17 -0500

    updating addPost method to include body parser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("views"));
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -14,6 +16,9 @@ app.get("/add", (req, res) => {
     res.render("add.ejs");
 });
 
+app.post("/addPost", (req, res) => {
+    console.log(req.body);
+});
 
 
 app.listen(port, () => {
